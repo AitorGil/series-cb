2aa361e5ba05a3f933fb2c6e575de0f147f1f322
Aitor_GC <aitorgilcallejo@gmail.com>
2018-12-22 21:18:12 +0100

    Trabajando en travis

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 /*
     Inicia la aplicación express que contiene el servidor.
 */
-const express = require("express");
+/*const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT);*/
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(2, 3));
