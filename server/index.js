4ec40c5638706bfa3e95f51addf47839b78d2af3
Aitor_GC <aitorgilcallejo@gmail.com>
2018-12-22 20:50:33 +0100

    Commit inicial

---
+++ server/index.js
@@ -0,0 +1,9 @@
+/*
+    Inicia la aplicación express que contiene el servidor.
+*/
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
