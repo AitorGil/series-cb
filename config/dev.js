5fcba288ed4572485a66f464a37ddedce5b0199b
Aitor_GC <aitorgilcallejo@gmail.com>
2018-12-26 15:07:44 +0100

    Add dev enviroment config to git

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb://root:HoLo33MM@ds143614.mlab.com:43614/series-cb-dev",
+  cookieKey:
+    "asfaspfjapwi23423japsjdasdjñaskdjaop234234wjdpajsdñasldmawokpoe87962"
+};
